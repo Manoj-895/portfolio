be8b6cb3becf0be4918be2add721c96f4ffd3765
Manoj kumar P Benni <manojpbimp@gmail.com>
2023-07-15 19:56:39 +0530

    portfolio changes V1

---
+++ src/pages/myskills.js
@@ -0,0 +1,42 @@
+export const mySkills = [
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhxGumO0GSQsTXzRh2OAYn2WnQmWP3bWV9jmd9yj0H&s",
+        Skill: "React .js",
+        description: "Have good knowledge in react concepts like React Hooks (UseState , UseEffect ..) React Redux , middlewares (thunk ,saga)"
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        Skill: "Javascript",
+        description: "Have strong foundation of javascript concepts and ES6 features of javascript"
+    },
+    {
+        image: "https://www.techrepublic.com/wp-content/uploads/2022/07/html-css-beginners.jpg?x27457",
+        Skill: "Html ans Css",
+        description: "Gained good experience in html and css working on differtent projects "
+    },
+    {
+        image: "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/full/nodejslogo.png",
+        Skill: "NodeJS",
+        description: "Have basic kowledge in Node and Express so as to build REST APIs"
+    }
+]
+
+export const myWork = [
+    {
+        projectName: "Building Microfront End components",
+        tasks: ["Used to build microfront end components which were used in different applications accross the organization"]
+    },
+    {
+        projectName: "Buy Now Pay Later",
+        tasks: ["We were creating a new application for merchant services for buy now pay later options",
+            "Played a key role by woring on UI developement and Api integration",
+            "Worked on tasks like UI logging Error handeling Unit test case writing for the components"]
+    },
+    {
+        projectName: "Costo product listing", 
+        tasks:[
+            "Working on product listing applicaton for merchants",
+            "Developing UI and integrating Apis"
+        ]
+    }
+]
